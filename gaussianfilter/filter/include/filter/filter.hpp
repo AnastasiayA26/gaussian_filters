ae0d46c333c7db7269c40f59c83f07917f8e67fb
AnastasiayA26 <75423962+AnastasiayA26@users.noreply.github.com>
2023-06-10 15:57:47 +0300

    Update filter.hpp

--- gaussianfilter/filter/include/filter/filter.hpp
+++ gaussianfilter/filter/include/filter/filter.hpp
@@ -3,6 +3,8 @@
 
 #include <opencv2/opencv.hpp>
 #include <vector>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
 
 class GaussianFilter : public cv::Algorithm {
 public:
@@ -12,17 +14,15 @@ public:
 
     cv::Mat gaussian_filter(const cv::Mat& input, double sigma);
 
-    double calculateGaussianError(const cv::Mat& input, const cv::Mat& output);
-
-    double calculateRecursiveGaussianDericheError(const cv::Mat& input, const cv::Mat& output);
+    void saveCoordinatesToFile(const std::string& filename, const std::vector<std::pair<double, double>>& coordinates);
 
     bool file_exists(const std::string& filename);
 
-    void visualizeData(const std::vector<std::pair<double, double>>& data);
+    void visualizeData(const std::vector<std::vector<std::pair<double, double>>>& data);
 
-    std::vector<std::pair<double, double>> calculateDeviations(const cv::Mat& image, const std::string& mode, double sigma);
+    std::vector<std::vector<std::pair<double, double>>> calculateDeviations(const cv::Mat& image, const std::string& mode, double sigma);
 
-    void compare_algorithms(const std::string& input_filename, double sigma, const std::string& algorithm_type, const std::string& visualization, const std::string& output_file_path1, const std::string& output_file_path2);
+    void compare_algorithms(const std::string& input_filename, double sigma, const std::string& algorithm_type, const std::string& visualization, const std::string& output_file_path1);
 };
 #endif // GAUSSIANFILTER_FILTERG_HPP
 
